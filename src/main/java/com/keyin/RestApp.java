0bd020f1e96c3d36c85c1d0dbafec8705020fc3f
Darla Ward <105027556+darlamward@users.noreply.github.com>
2023-07-25 00:21:45 -0230

    Added description and info.

--- src/main/java/com/keyin/RestApp.java
+++ src/main/java/com/keyin/RestApp.java
@@ -1,3 +1,10 @@
+// An uncomplicated REST API designed for an aviation-oriented data model.
+// Its purpose is to facilitate the administration of cities, passengers, airports, and aircrafts.
+// This program is designed to work in conjunction with the MySqlWorkbench database, which is referred to as "QAP2SDAT."
+// GitHub Link: https://github.com/darlamward/SDAT_REST_API
+// Completed on: July 25, 2023
+// Completed by Darla Ward.
+
 package com.keyin;
 
 import org.springframework.boot.SpringApplication;
